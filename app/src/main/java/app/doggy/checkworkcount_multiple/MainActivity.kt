1267c8c25a26c9f8e85fe3e4f0b5622e7b93baa6
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-05 20:41:45 +0900

    add: 左右のplus_buttonを実装

--- app/src/main/java/app/doggy/checkworkcount_multiple/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_multiple/MainActivity.kt
@@ -12,5 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        // 整数を管理する変数
+        var leftCount: Int = 0
+        var rightCount: Int = 0
+
+        // left_plus_buttonをクリックした時の処理
+        binding.leftPlusButton.setOnClickListener {
+            // left_countの値を1増やす
+            leftCount += 1
+
+            // left_countの値をleft_count_textに表示する
+            binding.leftCountText.text = leftCount.toString()
+        }
+
+        // right_plus_buttonをクリックした時の処理
+        binding.rightPlusButton.setOnClickListener {
+            // right_countの値を1増やす
+            rightCount += 1
+
+            // right_countの値をright_count_textに表示する
+            binding.rightCountText.text = rightCount.toString()
+        }
     }
 }
